36dc104b76b9160f69102dea54bd0f53c442d584
Aron John Bautista <701035287@GoTVDSB.ca>
2025-02-28 14:45:18 -0500

    Updatd organize

--- Topic 7.1 - Programming Tasks/Program.cs
+++ Topic 7.1 - Programming Tasks/Program.cs
@@ -7,38 +7,38 @@ namespace Topic_7._1___Programming_Tasks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Topic 7.1 - Programming Tasks");
-            Console.WriteLine("");
-            bool correct= false;
-            Console.WriteLine("Casino Game");
-            Console.WriteLine("Guess either 1.Heads or 2.Tails (Choose the number beside it or choose 1 to 2)");
             Random random = new Random();
             int coin;
-            coin = random.Next(1, 3);
-            Console.Write("Answer: ");
             int answer = 0;
+            bool done = false;
 
+            Console.WriteLine("Topic 7.1 - Programming Tasks");
             Console.WriteLine("");
-            while (correct!)
-            {
-                 int.TryParse(Console.ReadLine(), out answer);
+            Console.WriteLine("Casino Game");
 
+            while (!done) 
+            { 
+                coin = random.Next(1, 3);
+                Console.WriteLine("Guess either 1.Heads or 2.Tails (Choose the number beside it or choose 1 to 2)");
+                Console.Write("Answer: ");
+                int.TryParse(Console.ReadLine(), out answer);
+                
+                Console.WriteLine("");
                 if (answer == coin)
                 {
                     Console.WriteLine("Correct!");
-                    while (answer  != coin)
-                    {
-
-                    }
                 }
-                else 
-                {
-                    Console.WriteLine("Wrong! Type your answer again!");
 
-                    Console.WriteLine("Answer: ");
-                    Console.WriteLine();
-                }
+
             }
+
+
+            Console.WriteLine("Guess either 1.Heads or 2.Tails (Choose the number beside it or choose 1 to 2)");
+            Console.Write("Answer: ");
+            int.TryParse(Console.ReadLine(), out answer);
+            Console.WriteLine("");
+            
+            
+        }
         }
     }
-}
