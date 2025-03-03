2525eef964d1304a2fac26c68f19befb14ce4598
Aron John Bautista <701035287@GoTVDSB.ca>
2025-03-03 14:18:22 -0500

    wow

--- Topic 7.1 - Programming Tasks/Program.cs
+++ Topic 7.1 - Programming Tasks/Program.cs
@@ -9,7 +9,7 @@ namespace Topic_7._1___Programming_Tasks
         {
             Random random = new Random();
             int coin;
-            int answer = 0;
+            int answer, points = 0;
             bool done = false;
 
             Console.WriteLine("Topic 7.1 - Programming Tasks");
@@ -27,18 +27,29 @@ namespace Topic_7._1___Programming_Tasks
                 if (answer == coin)
                 {
                     Console.WriteLine("Correct!");
+                    Console.WriteLine("You gain one point!");
+                    Console.WriteLine("Points: ");
+                    Console.WriteLine(points + 1);
+                    
                 }
-
-
+                if (answer != coin)
+                {
+                    Console.WriteLine("Wrong");
+                    Console.WriteLine("You lose one point!");
+                    Console.WriteLine("Point: ");
+                    Console.WriteLine(points - 1);
+                    Console.WriteLine("");
+                }
             }
-
-
-            Console.WriteLine("Guess either 1.Heads or 2.Tails (Choose the number beside it or choose 1 to 2)");
-            Console.Write("Answer: ");
-            int.TryParse(Console.ReadLine(), out answer);
-            Console.WriteLine("");
-            
-            
-        }
+                    Console.WriteLine("Guess either 1.Heads or 2.Tails (Choose the number beside it or choose 1 to 2)");
+                    Console.Write("Answer: ");
+                    int.TryParse(Console.ReadLine(), out answer);
+                    Console.WriteLine("");
+            if (points == 0)
+            {
+                Console.WriteLine("Game over!");
+                done = true;
+            }
+        }  
         }
     }
